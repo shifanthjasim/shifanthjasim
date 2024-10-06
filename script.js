f24c10051c1de114672df35a35ecf7389a690af9
Shifanth Jasim <60750985+shifanthjasim@users.noreply.github.com>
2024-10-06 15:48:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-// Get the button
-const scrollUpButton = document.getElementById("scrollUp");
+// Select the "Back to Top" button
+const scrollUpButton = document.getElementById("btn-back-to-top");
 
 // Show the button when the user scrolls down 100px from the top
 window.onscroll = function() {
+    toggleScrollUpButton();
+};
+
+function toggleScrollUpButton() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         scrollUpButton.style.display = "block";
     } else {
         scrollUpButton.style.display = "none";
     }
-};
+}
 
 // When the user clicks on the button, scroll to the top of the document
 scrollUpButton.addEventListener("click", function() {
@@ -18,13 +22,13 @@ scrollUpButton.addEventListener("click", function() {
     });
 });
 
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
         });
     });
-
+});
